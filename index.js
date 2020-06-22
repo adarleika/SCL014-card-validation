379b8aa796d979c66963a9d86a1855222a042117
adarleika <49289372+adarleika@users.noreply.github.com>
2020-06-22 17:28:29 -0400

    Updates

---
+++ index.js
@@ -0,0 +1,142 @@
+import validator from './validator.js';
+
+
+//console.log(validator);
+
+
+//interaccion del DOM
+const formulario = document.querySelector('#formulario-tarjeta');
+formulario.inputNumero.addEventListener("keyup", (e) => {
+
+
+  //console.log (e);
+  //console.log (e.target.value);
+  //aqui ya podriamos ver el numero completo /console.log (e.target.value);
+
+    var valorNumeroArreglo = e.target.value;
+    //console.log (valorNumeroArreglo);
+
+    validator.isValid(valorNumeroArreglo);
+ 
+ });
+
+
+
+//const tarjeta = document.querySelector('#tarjeta'),
+const btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+//btnEnviarFormulario = document.querySelector('btn-enviar'),
+//formulario = document.querySelector('#formulario-tarjeta'),
+numeroTarjeta = document.querySelector('#tarjeta .numero'),
+nombreTarjeta = document.querySelector('#tarjeta .nombre'),
+logoMarca = document.querySelector('#logo-marca'),
+mesExpiracion = document.querySelector('#tarjeta .mes'),
+yearExpiracion = document.querySelector('#tarjeta .year');
+
+btnAbrirFormulario.addEventListener('click', () => {
+  btnAbrirFormulario.classList.toggle('active');
+  formulario.classList.toggle('active');
+
+
+});
+
+
+
+
+// selector del Mes
+for ( let i=1; i <= 12; i++){
+//console.log (i);
+let opcion= document.createElement ("option");
+opcion. value=i;
+opcion.innerText=i;
+formulario.selectMes.appendChild(opcion);
+
+}
+
+// selector del año
+const yearActual = new Date ().getFullYear ();
+for (let i=yearActual ; i<= yearActual+4; i++){
+//console.log (i);
+let opcion = document.createElement("option");
+opcion.value=i;
+opcion.innerText= i;
+formulario.selectYear.appendChild(opcion);
+
+}
+
+
+
+// input de la Tarjeta
+
+formulario.inputNumero.addEventListener("keyup", (e) => {
+
+//console.log (e);
+//aqui ya podriamos ver el numero completo /console.log (e.target.value);
+
+let valorInput = e.target.value;
+formulario.inputNumero.value = valorInput
+// ojo que aqui en los replace si influye las dobles comillas, por lo que hay que poner una sola comilla'' si poneer "" cambia el resultado 
+// elimiar espacios que esten en blanco
+.replace(/\s/g, '')
+// elimiar letras
+.replace(/\D/g, '')
+// ponemos este metodo para eliminar el ultimoespaciado de la cadena
+.trim();
+
+// es para visualizar en la pagina el numero de tarjeta   
+numeroTarjeta.textContent = valorInput;
+  var masked_str = valorInput;
+// cuando la persona borre lo escrito y que no se corra la palabra numero de tarjeta 
+if(valorInput == ''){
+  numeroTarjeta.textContent ='#### #### #### ####'
+   
+// descargar las imagenes de los logo
+logoMarca.innerHTML = '';
+}
+
+else{
+  
+validator.maskify(masked_str);
+
+}
+
+
+
+if(valorInput[0] == 4){
+  logoMarca.innerHTML = '';
+  const imagen = document.createElement('img');
+  imagen.src = '/img/visa.png';
+  logoMarca.appendChild(imagen);
+} else if(valorInput[0] == 5){
+  logoMarca.innerHTML = '';
+  const imagen = document.createElement('img');
+  imagen.src = '/img/mastercard.png';
+  logoMarca.appendChild(imagen);
+}
+
+});
+
+//  Input nombre de tarjeta
+formulario.inputNombre.addEventListener('keyup', (e) => {
+let valorInput = e.target.value;
+
+formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+nombreTarjeta.textContent = valorInput; 
+if(valorInput ==''){
+  nombreTarjeta.textContent = 'xxxxx xxxxx';
+  
+}
+
+
+});
+
+// para que cuando el usurio escriba la fecha se vea reflejada en la tarjeta 
+formulario.selectMes.addEventListener('change', (e) => {
+mesExpiracion.textContent = e.target.value;
+
+});
+
+// para que cuando el usurio escriba el año se vea reflejada en la tarjeta 
+formulario.selectYear.addEventListener('change', (e) => {
+yearExpiracion.textContent = e.target.value.slice(2);
+
+});
---
+++ validator.js
@@ -0,0 +1,87 @@
+const validator = {
+
+
+ 
+maskify:(valorInput) =>{
+
+  var valorNumeroArreglo = valorInput;
+  var digitos = valorNumeroArreglo.slice(-4);
+  var mask_simbolo = "#";
+  var masked_str = mask_simbolo.repeat(12) + digitos;
+  
+  document.querySelector(".numero").innerHTML = masked_str;
+  //console.log("este es el numero de tu tarjeta " + masked_str);
+
+ },
+ 
+
+ isValid:(valorInput) =>{
+
+  //var retornar = false;
+  //console.log(typeof valorInput);
+  let valorNumeroArreglo = valorInput.split("");
+  //var arrInvertido=valorNumeroArreglo.reverse();
+     //console.log('este es el reverso: '+arrInvertido);
+
+  var prod = 1;
+  var resul = 0;
+  var sum = 0;
+  var capa;
+  
+ 
+    //Recorro mi arreglo posición a posición para realizar el algoritmo de Luhn
+    for(var i = 0; i < valorNumeroArreglo.length; i++){
+     //Determino si una posición es par
+       if(i%2===0){
+         //Asigno a prod el valor del número en la posición i multiplicada por dos
+           prod=valorNumeroArreglo[i]*2;
+         //Si el prod es menor que 10 entra y se asigna ese valor en reemplazo de el elemento que esta en la posicion i del arreglo
+           if(prod<10){
+               valorNumeroArreglo[i]=prod;
+           }else{
+                // si no se cumplió la condición anterior entonces prod>=10 por lo tanto realizo la operación de acuerdo al
+               // algoritmo de luhn, que en este caso es sumar las cifras del prod
+               resul=Math.floor(prod/10)+(prod%10);
+               // Asigno el valor de mi resul al elemento en la posicion i del array
+               valorNumeroArreglo[i]=resul;
+           }
+       }
+       else{
+         //si la posicion es impar solo lo mulplico por 1 para convertirlo a tipo de dato numerico y escribo el mismo numero
+         valorNumeroArreglo[i]=valorNumeroArreglo[i]*1;
+       }
+   }
+   //muestro mi nuevo arreglo
+   //console.log(valorNumeroArreglo);
+    //sumo cada elemento del arreglo actual y lo almaceno en sum
+    for(i=0; i<valorNumeroArreglo.length; i++){
+     sum=sum+valorNumeroArreglo[i];
+    }
+    //muestro mi suma total
+    //console.log(sum);
+    //Pregunta si la suma del arreglo actual es divisible entre 10
+    if(sum%10===0){
+      //Si es divisible muestra que es un  tarjeta válida.
+       //return msg = "Tarjeta de crédito valida";
+       
+        capa = document.getElementById ("validacion")
+       //console.log("Tarjeta de Credito Valida");
+       capa.innerHTML = "Tarjeta de Credito Valida";
+       return true;
+      
+   }else{
+      //Si no es divisible entre 10 muestra tarjeta no válida
+       //return msg = "Tarjeta de crédito no valida";
+        capa = document.getElementById ("validacion")
+       //console.log("Tarjeta de Credito Invalida");
+       capa.innerHTML = "Tarjeta de Credito Invalida";
+       return false;
+   }
+   //return retornar;
+  }
+    //document.write('<br> '+ isValid(valorNumeroArreglo));
+    //console.log(isValid(valorInput));
+  //}
+};
+
+export default validator;
